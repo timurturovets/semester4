555ca1d24145e97bf6ecaae7d995ad42e758e079
teletraan <timurturovets@mail.ru>
2026-02-16 17:16:59 +0300

    fix

--- arithmetic/hw1/helpers/Recurrence.cs
+++ arithmetic/hw1/helpers/Recurrence.cs
@@ -34,8 +34,28 @@ public static class Recurrence
 
 public static class PoweredRecurrence
 {
-    public static (string asymptotic, double approximate) AsymptoticT(double x)
+    public static (string asymptotic, double approximate) AsymptoticT(double x, double alpha, double beta)
     {
-        return ("T(x) ~ 2^x, x -> +бесконечность", Math.Pow(2, x));
+        var r = alpha / beta;
+        string asymptotic;
+        double approx;
+        if (r < 1)
+        {
+            asymptotic = $"T(x) ~ x / (1 - {alpha}/{beta})";
+            approx = x / (1 - r);
+        }
+        else if (Math.Abs(r - 1) < Auxiliary.EPS)
+        {
+            asymptotic = $"T(x) ~ x * log_{beta}(x)";
+            approx = x * Math.Log(x) / Math.Log(beta);
+        }
+        else
+        {
+            var exp = 1 + Math.Log(r) / Math.Log(beta);
+            asymptotic = $"T(x) ~ x^{exp}";
+            approx = Math.Pow(x, exp);
+        }
+
+        return (asymptotic, approx);
     }
 }
--- arithmetic/hw1/task7/Program.cs
+++ arithmetic/hw1/task7/Program.cs
@@ -6,6 +6,20 @@ public class Task7
 {
     public static void Run(string[] args)
     {
+        double alpha;
+        Console.Write("Введите число альфа: ");
+        while (!double.TryParse(Console.ReadLine(), out alpha))
+        {
+            Console.Write("Некорректный ввод. Введите заново: ");
+        }
+        
+        double beta;
+        Console.Write("Введите число бета: ");
+        while (!double.TryParse(Console.ReadLine(), out beta))
+        {
+            Console.Write("Некорректный ввод. Введите заново: ");
+        }
+        
         double x;
         Console.Write("Введите x: ");
         while (!double.TryParse(Console.ReadLine(), out x))
@@ -13,8 +27,7 @@ public class Task7
             Console.Write("Некорректный ввод. Введите заново: ");
         }
         
-        var (asymptotic, approx) = PoweredRecurrence.AsymptoticT(x);
-        Console.WriteLine("Ведущий порядок роста - член 2^x.");
+        var (asymptotic, approx) = PoweredRecurrence.AsymptoticT(x, alpha, beta);
         Console.WriteLine($"{asymptotic}, T({x}) = {approx}");
     }
 }
