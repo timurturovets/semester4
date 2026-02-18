392d8bef1ff0a27758a6fef6a30f751f339d563e
teletraan <timurturovets@mail.ru>
2026-02-19 01:26:12 +0300

    probability hw1 task2

--- probability/hw1/Gmurman/Program.cs
+++ probability/hw1/Gmurman/Program.cs
@@ -8,11 +8,12 @@ public class Gmurman
         var innerRunners = new Dictionary<int, Runner>()
         {
             { 6, Task6.Run },
-            
+            { 8, Task8.Run },
         };
         
         Console.WriteLine("Задачи из пособия Гмурмана");
         Console.WriteLine("6. Куб распилен на 1000 кубиков...");
+        Console.WriteLine("8. В коробке 6 занумерованных кубиков...");
         Console.WriteLine("0. Выход");
         Console.WriteLine();
         
---
+++ probability/hw1/Gmurman/Task8.cs
@@ -0,0 +1,42 @@
+﻿namespace Gmurman;
+
+internal class Task8
+{
+    internal static void Run()
+    {
+        const int n = 6;
+        const int experiments = 1_000_000;
+
+        var success = 0;
+        var rnd = new Random();
+        
+        for (var i = 0; i < experiments; i++)
+        {
+            var cubes = Enumerable.Range(1, n).ToArray();
+            Shuffle(cubes, rnd);
+
+            if (IsStrictlyIncreasing(cubes)) success++;
+        }
+        
+        Console.WriteLine($"Вычисленная вероятность: {(double) success / experiments}");
+    }
+
+    private static void Shuffle(int[] cubes, Random rnd)
+    {
+        for (var i = cubes.Length - 1; i > 0; i--)
+        {
+            var j = rnd.Next(i + 1);
+            (cubes[i], cubes[j]) = (cubes[j], cubes[i]);
+        }
+    }
+
+    private static bool IsStrictlyIncreasing(int[] array)
+    {
+        for (var i = 1; i < array.Length; i++)
+        {
+            if (array[i] <= array[i - 1]) return false;
+        }
+
+        return true;
+    }
+}
